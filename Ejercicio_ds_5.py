226599663cd1ac87a66be1bcf689e4d680e1aee9
Ismael Plata <iplaros738@ieszaidinvergeles.org>
2026-09-24 14:27:17 +0200

    Ejercicio 5 datos simples

---
+++ Ejercicio_ds_5.py
@@ -0,0 +1,6 @@
+horasWeek = input("Cuantas horas trabajas a la semana? ")
+PagaHora = input("Cuanto cobras por hora? ")
+salarioS = int(horasWeek) * int(PagaHora)
+Salario = salarioS * 4
+print(f"Tu salario semanal es de : {salarioS} euros")
+print(f"Tu salario mensual es de : {Salario} euros")
